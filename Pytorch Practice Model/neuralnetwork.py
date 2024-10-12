15f1414e678ecb949e943249a4a86916b01455d5
jacob528 <jacoblleonart528@gmail.com>
2024-10-11 23:39:34 -0400

    Fixed environment and neural network shows images

---
+++ Pytorch Practice Model/neuralnetwork.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from matplotlib import pyplot
+import numpy as np
+import requests
+import pickle
+import torch
+import gzip
+
+DATA_PATH = Path("data")
+PATH = DATA_PATH / "mnist"
+
+PATH.mkdir(parents=True, exist_ok=True)
+
+URL = "https://github.com/pytorch/tutorials/raw/main/_static/"
+FILENAME = "mnist.pkl.gz"
+
+if not (PATH / FILENAME).exists():
+    content = requests.get(URL + FILENAME).content
+    (PATH / FILENAME).open("wb").write(content)
+
+with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
+    ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding="latin-1")
+
+
+pyplot.imshow(x_train[0].reshape((28, 28)), cmap="gray")
+# ``pyplot.show()`` only if not on Colab
+try:
+    import google.colab
+except ImportError:
+    pyplot.show()
+print(x_train.shape)
+
+x_train, y_train, x_valid, y_valid = map(
+    torch.tensor, (x_train, y_train, x_valid, y_valid)
+)
+n, c = x_train.shape
+print(x_train, y_train)
+print(x_train.shape)
+print(y_train.min(), y_train.max())
